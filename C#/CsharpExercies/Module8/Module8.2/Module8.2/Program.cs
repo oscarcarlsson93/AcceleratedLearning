c641518c09381cb272d486caf82eb1b03cb8200d
oscarcarlsson93 <oscargbcarlsson@gmail.com>
2018-12-03 17:00:42 +0100

    Modul 8: Två övningar med try-catch exceptions.

---
+++ C#/CsharpExercies/Module8/Module8.2/Module8.2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Module8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a file name: ");
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string filename = Console.ReadLine();
+                    File.CreateText(filename);
+                    Console.ResetColor();
+                    Console.WriteLine($"The file {filename} is now created");
+                    Console.WriteLine();
+                    break;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You are not authorized to create this file");
+                    Console.ResetColor();
+                }
+                //catch (IOException)
+                //{
+                //    Console.ForegroundColor = ConsoleColor.Red;
+                //    Console.WriteLine("Input output exception");
+                //    Console.ResetColor();
+                //}
+                catch (ArgumentException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Något är fel på filnamnet!");
+                    Console.ResetColor();
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("The filename is not valid");
+                    Console.ResetColor();
+                }
+            }
+
+
+
+
+        }
+    }
+}
---
+++ C#/CsharpExercies/Module8/Module8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Module8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The chocolate contains 24 pieces");
+            Console.Write("How many want to share? ");
+            decimal input = decimal.Parse(Console.ReadLine());
+            decimal pieces = 24;
+
+            try
+            {
+                decimal result = AntalBitar(input, pieces);
+                Console.WriteLine($"Everyone get {result:.##} pieces");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
+        }
+
+        private static decimal AntalBitar(decimal input, decimal pieces)
+        {
+            if (input < 0)
+            {
+                throw new ArgumentException("Felaktig inmatning");
+            }
+            if (input < 1)
+            {
+                throw new ArgumentException("Zero people can't divide a chocolate");
+            }
+           
+            return pieces / input;
+        }
+    }
+}
