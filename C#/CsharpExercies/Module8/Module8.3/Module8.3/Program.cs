c8224c75c42ba428ca8ab309021aeda2020e92ea
oscarcarlsson93 <oscargbcarlsson@gmail.com>
2018-12-04 10:47:57 +0100

    Lista med try&catch.

---
+++ C#/CsharpExercies/Module8/Module8.3/Module8.3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Module8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.Write("Enter a list of animals: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                string animals = Console.ReadLine();
+                Console.ResetColor();
+
+                try
+                {
+                    var animalList = ParseAnimals(animals);
+                    Console.WriteLine($"There are {animalList.Length} animals in the list");
+                    break;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+
+                string[] ParseAnimals(string animalsString)
+                {
+                        if (string.IsNullOrWhiteSpace(animalsString))
+                        {
+                            throw new ArgumentException("Animal string don't contain any letters");
+                        }
+                    string[] animalsList = animalsString.Split(',');
+                    foreach (var animal in animalsList)
+                    {
+
+                        if (animal.Length >= 15)
+                        {
+                            throw new ArgumentException($"This animal: '{animal}' contains too many letters");
+                        }
+                        if (!animal.All(c => Char.IsLetter(c)))
+                        {
+                            throw new ArgumentException($"Animal:{animal} contains invalid letters");
+                        }
+                        if (string.IsNullOrWhiteSpace(animal))
+                        {
+                            throw new ArgumentException("One of the animals don't contain any letters");
+                        }
+                    }
+                    return animalsList;
+                }
+            }
+        }
+    }
+}
