fce46df38484d00b1c60c3ddb5c5275ee5c56fcd
oscarcarlsson93 <oscargbcarlsson@gmail.com>
2018-12-04 11:47:43 +0100

    Se vad som händer i en fil; när en fil skapas, tas bort, namnges eller ändras

---
+++ C#/CsharpExercies/Module9/Module9.2/Module9.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Module9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watcher = new FileSystemWatcher();
+            watcher.Path = @"C:\Project\";
+            watcher.EnableRaisingEvents = true;
+
+            watcher.Created += FileCreated;
+            watcher.Renamed += FileRenamed;
+            watcher.Deleted += FileDeleted;
+            watcher.Changed += FileChanged;
+            
+            Console.WriteLine($"I'm wathcing this folder: {watcher.Path}");
+
+            Console.ReadKey();
+        }
+
+        private static void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File {e.Name} removed");
+        }
+
+        private static void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"File {e.Name} renamed");
+        }
+
+        private static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File {e.Name} created");
+        }
+
+        private static void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File {e.Name} changed");
+        }
+    }
+}
