b1d23e3c07eebb655d6c15f74ef468e2e07f83dd
oscarcarlsson93 <oscargbcarlsson@gmail.com>
2018-12-05 16:58:39 +0100

    Nya test-övningar

---
+++ C#/CsharpExercies/NumberListToNumberListTests/MethodsAndLists/MethodsAndLists.Core/NumberListToStringList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace MethodsAndLists.Core
+{
+    public class NumberListToStringList
+    {
+
+        public List<string> LongOrShort(List<int> heightList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> NegativeNumbersIsZip_PositiveNumbersIsZap_ZeroIsBoing(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> AddStarsToNumbersHigherThan1000(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> AddStars(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
