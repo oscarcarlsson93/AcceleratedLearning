0619d3d312235da4562027834ab3fe8e9a90b62e
oscarcarlsson93 <oscargbcarlsson@gmail.com>
2019-02-14 16:46:16 +0100

    TRELLO API

---
+++ C#/Checkpoints/Checkpoint09/TrelloTest/TrelloService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrelloTest
+{
+    class TrelloService
+    {
+
+        private async Task<string> Get(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            using (HttpContent content = response.Content)
+            {
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception(response.ReasonPhrase);
+
+                return await content.ReadAsStringAsync();
+            }
+        }
+
+        private async Task<string> Post(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.PostAsync(url, null))
+            using (HttpContent content = response.Content)
+            {
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception(response.ReasonPhrase);
+
+                return await content.ReadAsStringAsync();
+            }
+        }
+
+
+    }
+}
